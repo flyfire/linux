d7d45083261f193313164059d1aa3fbc36f34c12
Cong Wang <amwang@redhat.com>
2011-11-25 22:57:25 +0800

    powerpc: remove km_type definitions
    Signed-off-by: Cong Wang <amwang@redhat.com>

--- arch/powerpc/include/asm/kmap_types.h
+++ arch/powerpc/include/asm/kmap_types.h
@@ -10,25 +10,7 @@
  * 2 of the License, or (at your option) any later version.
  */
 
-enum km_type {
-	KM_BOUNCE_READ,
-	KM_SKB_SUNRPC_DATA,
-	KM_SKB_DATA_SOFTIRQ,
-	KM_USER0,
-	KM_USER1,
-	KM_BIO_SRC_IRQ,
-	KM_BIO_DST_IRQ,
-	KM_PTE0,
-	KM_PTE1,
-	KM_IRQ0,
-	KM_IRQ1,
-	KM_SOFTIRQ0,
-	KM_SOFTIRQ1,
-	KM_PPC_SYNC_PAGE,
-	KM_PPC_SYNC_ICACHE,
-	KM_KDB,
-	KM_TYPE_NR
-};
+#define KM_TYPE_NR 16
 
 /*
  * This is a temporary build fix that (so they say on lkml....) should no longer
